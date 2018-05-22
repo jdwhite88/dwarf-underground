2c42d6cde0223016760d8af67960e93ce13ba2b3
jdwhite88 <white570@purdue.edu>
2018-05-21 22:50:55 -0400

    Render Other Articles and footer as React component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header';
 import Article from './Article';
 import Ad from './Ad';
+import OtherArticles from './OtherArticles';
 
 class App extends Component {
   render() {
@@ -12,8 +13,12 @@ class App extends Component {
         <main className="expanded row">
           <Article />
           <Ad />
+          <OtherArticles />
         </main>
-
+        <footer class="expanded row">
+          <h6>&copy; 2017 Erebor Industries</h6>
+          <h6 class="float-right">All rights reserved</h6>
+        </footer>
       </div>
     );
   }
