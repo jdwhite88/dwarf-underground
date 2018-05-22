c01c4192b1ae135374b8d728965cffce76f17658
jdwhite88 <white570@purdue.edu>
2018-05-22 00:35:13 -0400

    Move footer into separate React component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from './Header';
 import Article from './Article';
 import Ad from './Ad';
 import OtherArticles from './OtherArticles';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+
         <main className="expanded row">
           <Article />
           <Ad />
@@ -37,10 +39,8 @@ class App extends Component {
             },
           ]} />
         </main>
-        <footer className="expanded row">
-          <h6>&copy; 2017 Erebor Industries</h6>
-          <h6 className="float-right">All rights reserved</h6>
-        </footer>
+
+        <Footer />
       </div>
     );
   }
